4e69fee52f1b07746082a8790ece453662fe3bcb
satya-p14 <vikkisatya@gmail.com>
2025-04-17 19:43:07 +0530

    Create findLargestPrimeNumber.js

---
+++ findLargestPrimeNumber.js
@@ -0,0 +1,25 @@
+// findLargestPrimeNumber in array
+
+function findLargestPrimeNumber(arr) {
+    let maxPrime = null;
+    for (let num of arr) {
+        if (isPrime(num) && num > maxPrime) {
+            maxPrime = num;
+        }
+    }
+    return maxPrime;
+}
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num === 2) return true;
+    if (num % 2 === 0) return false;
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+var arr = [1, 5, -7, 12, 7];
+console.log(findLargestPrimeNumber(arr));
+
